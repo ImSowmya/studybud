849ef560966368f08bffcd39f5dceae3ae07dfba
Sowmya-predigle <sowmya.muthuramalingam@predigle.com>
2026-03-08 13:42:12 +0530

    Views URLs and Templates added

--- base/views.py
+++ base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+rooms=[
+    {"id":1,"name":"Python"},
+    {"id":2,"name":"Design"},
+    {"id":3,"name":"AI ML"},
+]
+
+def home(request):
+    return render(request, 'home.html', {"rooms": rooms})
+def room(request):
+    return render(request, 'room.html')
